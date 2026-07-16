8b4d6d0dd2baa69cb53b27956a3f6ad11a4eef5a
Puneet Kaur Jattana <52742666+puneet601@users.noreply.github.com>
2026-07-16 14:59:11 +0530

    Add: largest-rectangle-in-histogram - submission-3

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-3.java
@@ -0,0 +1,56 @@
+class Solution {
+    class Pair{
+        int val;
+        int freq;
+        Pair(int val, int freq){
+            this.val=val;
+            this.freq=freq;
+        }
+    }
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int start = -1, end = -1;
+        int max = 0;int curr;
+        Stack<Pair> st = new Stack<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[st.peek().val] > heights[i]) {
+                Pair top = st.pop();
+                
+                count+=top.freq;
+                if (end < top.val) {
+                    end = top.val;
+                }
+                start = top.val;
+                curr = (top.freq + end - start) * heights[top.val];
+                
+                max = max > curr ? max : curr;
+                // System.out.println(top.val+" "+top.freq+" "+max+" "+curr+" "+heights[top.val]);
+            }
+            
+            st.push(new Pair(i,count+1));
+            count=0;
+        }
+        end = -1;start=-1;
+        while (!st.isEmpty()) {
+            Pair top = st.pop();
+            // System.out.println(top.val+" "+top.freq);
+            if (end == -1) {
+                end = top.val;
+            }
+            start = top.val;
+            curr = (top.freq+ end - start) * heights[top.val];
+            max = max > curr ? max : curr;
+        }
+        return max;
+    }
+}
+// 7 1 7 2 2 4
+// 0 1
+/*
+heights=[3,6,5,7,4,8,1,0]
+
+,1
+5,2
+0,1
+*/
